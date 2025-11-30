b862672a7e654137d43c29cfc053596a0c78b51c
Aditya <adityathakur00199.8@gmail.com>
2025-11-30 16:59:04 +0530

    little things matters or does it ?

--- Tender-chain/src/Components/Home.jsx
+++ Tender-chain/src/Components/Home.jsx
@@ -7,11 +7,10 @@ function Home() {
     return (
         <div className='h-screen w-full bg-neutral-900'>
             {/* Navbar */}
-            <nav className='bg-neutral-900 text-white flex justify-between items-center py-4 fixed top-0 left-0 w-full z-10 px-8'>
+            <nav className='bg-neutral-900 flex justify-between items-center py-4 fixed top-0 left-0 w-full z-10 px-8'>
                 <a href='/' className='flex items-center gap-2'>
                     <h1 className='text-2xl font-["Montserrat"] font-semibold tracking-tight text-green-400'>Tender-Chain</h1>
                 </a>
-
                 <div className='flex justify-center items-center gap-8'>
                     <button onClick={() => navigate("/login")} className='rounded-xl text-green-400 flex justify-center items-center gap-3 px-7 py-2 cursor-pointer transition-all duration-200 hover:text-black font-semibold border-2 border-green-400 hover:bg-green-400'>
                         <svg xmlns="http://www.w3.org/2000/svg" 
@@ -33,16 +32,18 @@ function Home() {
             </nav>
 
             {/* Hero Section */}
-            <div className='flex flex-col justify-center items-center w-full h-full pt-20'>
-                <div className='bg-green-400 w-full flex flex-col justify-center items-center py-20 px-4'>
+            <div className='flex flex-col justify-center items-center w-full h-full pt-20 gap-20'>
+                <div className='bg-neutral-900 text-neutral-900 py-30 px-4 w-full '>
+                    <h1>badbadbdbdubdwudvWUBCUWBCUWBCUAWCBWCHBWUCB<br/>r</h1>
+                </div>
+                    <div className='bg-green-400 w-full flex flex-col justify-center items-center py-25 px-4'>
                     <div className='text-center'>
                         <h1 className='text-6xl font-bold text-black font-["Playfair_Display"] tracking-tight'>Tender on Chain <br /> Is The New Name</h1>
-                        <p className='text-black mt-6 text-xl font-["Montserrat"] tracking-normal font-semibold'>Blockchain-powered decentralized tender system ensuring fair allocation,<br /> transparent processes, and complete accountability in government procurement.</p>
+                        <p className='text-black text-xl font-["Montserrat"] tracking-normal font-semibold'>Blockchain-powered decentralized tender system ensuring fair allocation,<br /> transparent processes, and complete accountability in government procurement.</p>
                     </div>
                 </div>
-
-                <div className='w-full flex justify-center items-center mt-20 px-4'>
-                    <button onClick={ () => window.open("https://sepolia.etherscan.io/address/0x0cf5ffb83022f9c5a2b7417fea77e6c1009d79e6#code", "_blank") } class="inline-flex items-center gap-2 bg-neutral-700 hover:bg-neutral-600 mt-2 px-4 py-2 rounded-full cursor-pointer">
+                <div className='w-full flex justify-center items-center py-30 bg-neutral-900'>
+                    <button onClick={ () => window.open("https://sepolia.etherscan.io/address/0x0cf5ffb83022f9c5a2b7417fea77e6c1009d79e6#code", "_blank") } class="inline-flex items-center gap-2 bg-neutral-700 hover:bg-neutral-600 px-4 py-2 rounded-full cursor-pointer">
                         <span class="relative flex w-3 h-3">
                         <span class="inline-flex absolute bg-green-500 opacity-100 rounded-full w-full h-full animate-ping"></span>
                         <span class="inline-flex relative bg-green-400 rounded-full w-3 h-3"></span>
@@ -50,9 +51,11 @@ function Home() {
                         <span class="font-medium text-green-400 ">Blockchain Network Active</span>
                     </button>
                 </div>
+                <div className='bg-red-300 w-full '>
+                    this is the demo div
+                </div>
             </div>
-
-
+            {/* Features Section */}
         </div>
     )
 }
